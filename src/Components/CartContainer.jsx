c7250a1d0e1990671280c2fe6dc579758cccd9cd
unknown <revchenkofront@gmail.com>
2021-06-17 13:41:34 +0300

    addet totalrice

--- src/Components/CartContainer.jsx
+++ src/Components/CartContainer.jsx
@@ -3,19 +3,26 @@ import { connect } from 'react-redux'
 import { removeFromCart } from '../Redux/Reducers/cartReducer'
 
 function Cart(props) {
+
+    let totalPrice = 0
+    
+    props.items.map(book => {
+        totalPrice = totalPrice + book.price
+    });
+   
     
 
-    console.log(props)
-    return (
+     return (
 
         <div className='CartContainer'>
             {props.items.map(item => <div className="Card-Item">
-                <img src={item.image}/>
-                <h3>{item.title}</h3> 
+                <img src={item.image} />
+                <h3>{item.title}</h3>
                 <h4>{item.price}</h4>
-                <button onClick={()=>props.removeFromCart(item.id)}>Удалить</button>
-            </div>)}     
-            <button>Оплатить</button>       
+                <button onClick={() => props.removeFromCart(item.id)}>Удалить</button>
+            </div>)}
+            <h3>Total price {totalPrice}</h3>
+            <button>Оплатить</button>
         </div>
 
     )
@@ -28,7 +35,7 @@ let mapStateToProps = (state) => {
     }
 }
 
-let CartContainer = connect(mapStateToProps, {removeFromCart})(Cart)
+let CartContainer = connect(mapStateToProps, { removeFromCart })(Cart)
 
 
 export default CartContainer
